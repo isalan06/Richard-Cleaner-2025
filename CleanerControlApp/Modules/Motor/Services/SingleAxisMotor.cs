6f3ed39666561172dbb0d1f983522832b803c1aa
isalan06 <isalan06@gmail.com>
2026-05-22 16:00:07 +0800

    improve GetInPos(0)

--- CleanerControlApp/Modules/Motor/Services/SingleAxisMotor.cs
+++ CleanerControlApp/Modules/Motor/Services/SingleAxisMotor.cs
@@ -335,6 +335,7 @@ namespace CleanerControlApp.Modules.Motor.Services
             if (_plcService != null)
             {
                 result = (Position == (_moduleMotor.Positions != null ? _moduleMotor.Positions[position] : 0) || _sim_pass_motor);
+                if (position == 0 && !result && Position == 0) result = true; // after home, shuttle X,Z will be 0, it is not P1-Original Position, but it can continue to move
 
             }
 
